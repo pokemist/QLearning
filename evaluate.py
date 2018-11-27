44bd98f8089215fba01e662b6b160d1ef60d89c6
paullee73 <paulleechat3@gmail.com>
2018-11-27 12:55:47 -0500

    first commit

--- evaluate.py
+++ evaluate.py
@@ -6,8 +6,8 @@ from functions import *
 import sys
 
 if len(sys.argv) != 3:
-	print "Usage: python evaluate.py [stock] [model]"
-	exit()
+    print("Usage: python evaluate.py [stock] [model]")
+    exit()
 
 stock_name, model_name = sys.argv[1], sys.argv[2]
 model = load_model("models/" + model_name)
@@ -22,28 +22,30 @@ state = getState(data, 0, window_size + 1)
 total_profit = 0
 agent.inventory = []
 
-for t in xrange(l):
-	action = agent.act(state)
+for t in range(l):
+    action = agent.act(state)
+    print(state)
 
-	# sit
-	next_state = getState(data, t + 1, window_size + 1)
-	reward = 0
+    # sit
+    next_state = getState(data, t + 1, window_size + 1)
+    reward = 0
 
-	if action == 1: # buy
-		agent.inventory.append(data[t])
-		print "Buy: " + formatPrice(data[t])
+    if action == 1:  # buy
+        agent.inventory.append(data[t])
+        print("Buy: " + formatPrice(data[t]))
 
-	elif action == 2 and len(agent.inventory) > 0: # sell
-		bought_price = agent.inventory.pop(0)
-		reward = max(data[t] - bought_price, 0)
-		total_profit += data[t] - bought_price
-		print "Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price)
+    elif action == 2 and len(agent.inventory) > 0:  # sell
+        bought_price = agent.inventory.pop(0)
+        reward = max(data[t] - bought_price, 0)
+        total_profit += data[t] - bought_price
+        print("Sell: " + formatPrice(data[t]) +
+              " | Profit: " + formatPrice(data[t] - bought_price))
 
-	done = True if t == l - 1 else False
-	agent.memory.append((state, action, reward, next_state, done))
-	state = next_state
+    done = True if t == l - 1 else False
+    agent.memory.append((state, action, reward, next_state, done))
+    state = next_state
 
-	if done:
-		print "--------------------------------"
-		print stock_name + " Total Profit: " + formatPrice(total_profit)
-		print "--------------------------------"
+    if done:
+        print("--------------------------------")
+        print(stock_name + " Total Profit: " + formatPrice(total_profit))
+        print("--------------------------------")
